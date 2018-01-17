dcd4aba6dac18ead202f13ea1509cff327e0f57e
dml92 <d.morenole@gmail.com>
2018-01-17 20:56:52 +0100

    Force chromedriver version 2.34
    In order to test if this fixes the integration problem with TravisCI, version 2.34 of chromedriver is  forced

--- src/test/java/com/burgerjavis/mvc/BurgerJavisMVCLoginTest.java
+++ src/test/java/com/burgerjavis/mvc/BurgerJavisMVCLoginTest.java
@@ -40,7 +40,7 @@ public class BurgerJavisMVCLoginTest {
 	private StringBuffer verificationErrors = new StringBuffer();
 	
 	@BeforeClass public static void setupClass() {
-		ChromeDriverManager.getInstance().setup();
+		ChromeDriverManager.getInstance().version("2.34").setup();
 		context = (ApplicationContext) SpringApplication.run(BurgerJavisServerApplication.class);
 	} 
 
